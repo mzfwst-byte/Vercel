d5216968192552b397d31d309573a78843420e9d
mzfwst-byte <mzfwst@gmail.com>
2026-01-22 18:51:30 +0900

    Update page.js

--- app/map/page.js
+++ app/map/page.js
@@ -1,29 +1,11 @@
 'use client'
 
-import { useEffect } from 'react'
-import L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
+import dynamic from 'next/dynamic'
 
-export default function MapPage() {
-  useEffect(() => {
-    const lat = 35.67797599475285
-    const lng = 139.66690577010075
+const Map = dynamic(() => import('./map-client'), {
+  ssr: false
+})
 
-    const map = L.map('map').setView([lat, lng], 13)
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19
-    }).addTo(map)
-
-    function draw(deg) {
-      const r = deg * Math.PI / 180
-      const d = 0.06
-      return [lat + d * Math.cos(r), lng + d * Math.sin(r)]
-    }
-
-    L.polyline([[lat, lng], draw(250)], { color: 'red' }).addTo(map)
-    L.polyline([[lat, lng], draw(200)], { color: 'blue' }).addTo(map)
-  }, [])
-
-  return <div id="map" style={{ height: '100vh', width: '100vw' }} />
+export default function Page() {
+  return <Map />
 }
