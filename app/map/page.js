b651975fd6e351219abc924c1c37ca2e65214d4c
mzfwst-byte <mzfwst@gmail.com>
2026-01-29 13:08:13 +0900

    Update page.js

--- app/map/page.js
+++ app/map/page.js
@@ -1,11 +1,9 @@
-'use client'
-
 import dynamic from 'next/dynamic'
 
-const Map = dynamic(() => import('./map-client'), {
+const MapClient = dynamic(() => import('./map-client'), {
   ssr: false
 })
 
 export default function Page() {
-  return <Map />
+  return <MapClient />
 }
